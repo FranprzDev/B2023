e9d520fe2a96dbb233d3a8b2d98a009ffb83e61f
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2023-12-08 12:43:05 -0300

    feat: :sparkles: add createStudent endpoint

--- clase-03/controllers/student.controllers.js
+++ clase-03/controllers/student.controllers.js
@@ -2,14 +2,29 @@ const Student = require('../models/Student')
 
 const getAllStudents = async (req, res, next) => {
     try {
-        const courses = await Student.find({})
+        const students = await Student.find({})
 
-        res.json(courses)
+        res.json(students)
+    } catch (err) {
+        next(err)
+    }
+}
+
+const createStudent = async (req, res, next) => {
+    try {
+        const { firstName, lastName } = req.body
+
+        const student = new Student({ firstName, lastName })
+        await student.save()
+
+        res.status(201)
+        res.json(student)
     } catch (err) {
         next(err)
     }
 }
 
 module.exports = {
-    getAllStudents
+    getAllStudents,
+    createStudent
 }
--- clase-03/routes/student.routes.js
+++ clase-03/routes/student.routes.js
@@ -1,8 +1,15 @@
-const { getAllStudents } = require('../controllers/student.controllers')
+const { getAllStudents, createStudent } = require('../controllers/student.controllers')
 const { errorMiddleware } = require('../middlewares/common.middleware')
+const { validateStudentData } = require('../middlewares/student.middleware')
 
 const studentRouter = require('express').Router()
 
 studentRouter.get('/', getAllStudents, errorMiddleware)
+
+studentRouter.post('/',
+    validateStudentData,
+    createStudent,
+    errorMiddleware
+)
 
 module.exports = studentRouter
